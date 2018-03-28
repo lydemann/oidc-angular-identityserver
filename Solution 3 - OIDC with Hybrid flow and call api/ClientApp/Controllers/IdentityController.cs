fa36d0eba29a72a81f876934e8d3efdd6a89fb43
Christian Lüdemann <christianlydemann@gmail.com>
2018-03-28 18:06:43 +0200

    setup hybrid flow with api

---
+++ Solution 3 - OIDC with Hybrid flow and call api/ClientApp/Controllers/IdentityController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ClientApp.Models;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ClientApp.Controllers
+{
+    public class IdentityController : Controller
+    {
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var userClaimsVM = new UserClaimsVM();
+            var userClaimsWithClientCredentials = await GetUserClaimsFromApiWithClientCredentials();
+            userClaimsVM.UserClaimsWithClientCredentials = userClaimsWithClientCredentials.IsSuccessStatusCode ? await userClaimsWithClientCredentials.Content.ReadAsStringAsync() : userClaimsWithClientCredentials.StatusCode.ToString();
+
+            var userClaimsWithAccessToken = await CallApiUsingUserAccessToken();
+            userClaimsVM.UserClaimsWithAccessToken = userClaimsWithAccessToken.IsSuccessStatusCode ? await userClaimsWithAccessToken.Content.ReadAsStringAsync() : userClaimsWithAccessToken.StatusCode.ToString();
+
+            return View(userClaimsVM);
+        }
+
+        private async Task<HttpResponseMessage> GetUserClaimsFromApiWithClientCredentials()
+        {
+            // discover endpoints from metadata
+            var oidcDiscoveryResult = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (oidcDiscoveryResult.IsError)
+            {
+                Console.WriteLine(oidcDiscoveryResult.Error);
+                throw new HttpRequestException(oidcDiscoveryResult.Error);
+            }
+
+            // request token
+            var tokenClient = new TokenClient(oidcDiscoveryResult.TokenEndpoint, "clientApp", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("resourceApi");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                throw new HttpRequestException(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/identity");
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> CallApiUsingUserAccessToken()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+            
+            return await client.GetAsync("http://localhost:5001/api/identity");
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+    }
+}
---
+++ Solution 3 - OIDC with Hybrid flow and call api/ClientApp/Startup.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ClientApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+                    options.Authority = "http://localhost:5000";
+
+                    options.ClientId = "mvc";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code id_token";
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.RequireHttpsMetadata = false;
+
+                    // Request these scopes
+                    options.Scope.Add("resourceApi");
+                    options.Scope.Add("offline_access");
+
+                });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
